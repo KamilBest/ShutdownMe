b87bf94e5bb23f8c42bfdce6a857f86f9995858a
KamilBest <kamil.besst@gmail.com>
2017-04-12 23:49:34 +0200

    Added displaying current time

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+
+    @FXML
+    private TextField currentTimeField;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Runnable runnable = new CurrentTime(currentTimeField);
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
 }
---
+++ src/sample/CurrentTime.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by kamil on 4/12/17.
+ */
+public class CurrentTime implements Runnable {
+    @FXML
+    private TextField currentTimeField;
+    public CurrentTime(TextField currentTimeField)
+    {
+        this.currentTimeField=currentTimeField;
+    }
+
+
+
+    @Override
+        public void run() {
+            try {
+                while(true){
+                    SimpleDateFormat sdate = new SimpleDateFormat("HH:mm:ss");
+                    String dateString = sdate.format(new Date());
+                    Platform.runLater(() -> currentTimeField.setText(dateString));
+                    Thread.sleep(1000);
+                }
+            } catch (Exception e) {
+            }
+        }
+}
