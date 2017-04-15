7b2459a623c87673f9f6e7594a0d1a91b5753aed
KamilBest <kamil.besst@gmail.com>
2017-04-15 20:22:31 +0200

    Code in all classes has been refactored.

--- src/sample/Shutdown.java
+++ src/sample/Shutdown.java
@@ -3,10 +3,18 @@ package sample;
 import java.io.IOException;
 
 /**
+ * Shutdown class
+ * Responsible for setting and cancelling shutdown.
+ * <p>
  * Created by kamil on 4/12/17.
  */
 public class Shutdown {
 
+    /**
+     * Sets shutdown
+     *
+     * @param value - value in hours from slider
+     */
     public void setShutdown(double value) {
         try {
             Process processShutdown = Runtime.getRuntime().exec("shutdown -s -t " + (value * 3600));
@@ -15,6 +23,9 @@ public class Shutdown {
         }
     }
 
+    /**
+     * Cancels shutdown
+     */
     public void cancelShutdown() {
         try {
             Process processCancel = Runtime.getRuntime().exec("shutdown -a");
